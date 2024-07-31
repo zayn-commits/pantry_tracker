ea5a1413e1856abcc0797396df0923d0128519a3
Zaynerrr <zaynlatchman@gmail.com>
2024-07-30 23:52:06 -0400

    Returning Name and Count to Pantry

--- app/page.js
+++ app/page.js
@@ -1,5 +1,44 @@
+'use client'
 import Image from "next/image";
+import{useState, useEffect} from 'react'
+import {firestore} from '@/firebase'
+import { Box, Typography } from "@mui/material";
+import { collection, getDocs, query } from "firebase/firestore";
 
 export default function Home() {
-  return <>Hello World</>
+  const [pantry, setPantry] = useState([])
+  const [open, setOpen] = useState(false)
+  const [itemName, setItemName] = useState('')
+
+  const updateInventory = async () => {
+    const snapshot = query(collection(firestore, 'pantry'))
+    const docs = await getDocs(snapshot)
+    const inventoryList = []
+    docs.forEach((doc)=>{
+      inventoryList.push({
+        name: doc.id,
+        ...doc.data(),
+      })
+    })
+    setPantry(inventoryList)
+    console.log(inventoryList)
+  }
+
+  useEffect(()=>{
+    updateInventory()
+  },[])
+  
+  return(
+    <Box>
+      <Typography variant="h1">Pantry Tracker</Typography>
+      {
+        pantry.forEach((item)=>{
+          return(<>
+          {item.name}
+          {item.count}
+          </>)
+        })
+      }
+    </Box>
+    )
 }
